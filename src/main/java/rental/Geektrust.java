0d4b56402333297ddea4569d3af4325e490ae097
rakeshranjan001 <rksh.ranjan49@gmail.com>
2022-09-24 02:23:13 +0530

    commit test

--- src/main/java/rental/Geektrust.java
+++ src/main/java/rental/Geektrust.java
@@ -7,7 +7,6 @@ import java.io.File;
 public class Geektrust {
     public static void main(String[] args) throws Exception {
         String filePath = args[0];
-      //  String filePath = "C:\\Users\\rkshr\\IdeaProjects\\rakesh_vehicle_rental\\vehicle\\src\\main\\resources\\sample_input";
         FileInputProcessor fileInputProcessor = new FileInputProcessor(new File(filePath));
         fileInputProcessor.process();
     }
